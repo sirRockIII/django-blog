f537ef66a05fcdb9243f768ec913c4469598e2f8
sirRockIII <110748701+sirRockIII@users.noreply.github.com>
2023-05-17 16:29:49 -0500

    Completed lesson 6.

---
+++ blogging/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from blogging.views import stub_view, list_view
+
+
+urlpatterns = [
+    path('', list_view, name="blog_index"),
+    path('posts/<int:post_id>', stub_view, name='blog_detail'),
+    path('category', list_view, name='blog_detail')
+]
